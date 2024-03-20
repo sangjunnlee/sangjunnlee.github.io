d03abcb1ff9a98615879b0739a9e46ddc9322cbd
sangjunnlee <134983124+sangjunnlee@users.noreply.github.com>
2024-03-20 12:19:20 +0000

    Feat: Initializing Project

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './Home';
+import Header from './Header';
+import Todo from './Todo';
+
+function App() {
+    return <div>
+        <Header />
+        <Home />
+        <Todo />
+    </div>
+}
+export default App;
