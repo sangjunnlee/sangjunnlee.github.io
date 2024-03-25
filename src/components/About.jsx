174347b43238af9c0ddd693cf5792bdb1b5f278a
sangjunnlee <134983124+sangjunnlee@users.noreply.github.com>
2024-03-25 06:39:51 +0000

    Feat: added image carousel about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,19 @@
 import React, {useState} from "react";
+import "../style/About.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import profileImage from "../images/portrait.jpeg"
 
 function About() {
     return (
         <div>
             <h1>About</h1>
+            <Carousel className="crsl" autoPlay infiniteLoop centerMode showArrows={false} showStatus={false} showThumbs={false}> 
+                <img src={profileImage} alt="image 1"/>
+                <img src={profileImage} alt="image 2"/>
+                <img src={profileImage} alt="image 3"/>
+                <img src={profileImage} alt="image 4"/>
+            </Carousel>
         </div>
     )
 }
