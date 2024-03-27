6e19c81300f060798f7d2a190d792953a48deeef
sangjunnlee <sjlee9920@gmail.com>
2024-03-27 21:22:05 +0900

    Feat: Changed layout of timeline to appear on the same page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,6 +36,32 @@ function About() {
             <div className="about-content">
                 <h1 className="title" data-aos='slide-right' style={{textAlign: 'center', marginBottom: '5%'}}>Who am I?</h1>
                 <div className="core">
+                <div className="timeline">
+                    <div class="timeline-container left-container">
+                        <img src="/images/highschool-grad.jpg" alt="temp" />
+                        <div class="text-box">
+                            <h2>Guatemala</h2>
+                            <small>2021-2022</small>
+                            <span class="left-container-arrow"></span>
+                        </div>
+                    </div>
+                    <div class="timeline-container right-container">
+                        <img src="/images/highschool-grad.jpg" alt="temp" />
+                        <div class="text-box">
+                            <h2 >USA</h2>
+                            <small>2021-2022</small>
+                            <span class="right-container-arrow"></span>
+                        </div>
+                    </div>
+                    <div class="timeline-container left-container">
+                        <img src="/images/highschool-grad.jpg" alt="temp" />
+                        <div class="text-box">
+                            <h2 >Korea</h2>
+                            <small>2021-2022</small>
+                            <span class="left-container-arrow"></span>
+                        </div>
+                    </div>
+                </div>
                     <Carousel className="crsl" autoPlay infiniteLoop showIndicators={false} showArrows={false} showStatus={false} showThumbs={false}> 
                         {imagePaths.map((imagePath, index) => (
                             <img key={index} src={imagePath} alt={'Image ${index}'}/>
@@ -45,35 +71,6 @@ function About() {
                     I’m Kelly Hu (hoo, like an owl 🦉), a junior at UC Berkeley studying Electrical Engineering & Computer Science (EECS) with a certification in Design Innovation. As a proud Canadian (I'm from Toronto!), ex-bioengineering major, and occasional video-maker, I try to be intentional about exploring a hodgepodge of disciplines that help inform my passion for design.
                     Always growing and reflecting, I'm currently really interested in designing for human connection and social impact. Creating delightful yet intuitive and highly polished interfaces that are purposeful and well-intentioned is what excites me!
                     </p>
-                </div>
-                <div className="timeline">
-                    <div class="timeline-container left-container">
-                        <img src="/images/highschool-grad.jpg" alt="temp" />
-                        <div class="text-box">
-                            <h2>Guatemala</h2>
-                            <small>2021-2022</small>
-                            <p>saklfjdlkafajksfjadskfsdfjaskldkj</p>
-                            <span class="left-container-arrow"></span>
-                        </div>
-                    </div>
-                    <div class="timeline-container right-container">
-                        <img src="/images/highschool-grad.jpg" alt="temp" />
-                        <div class="text-box">
-                            <h2 >USA</h2>
-                            <small>2021-2022</small>
-                            <p >saklfjdlkafajksfjadskfsdfjaskldkj</p>
-                            <span class="right-container-arrow"></span>
-                        </div>
-                    </div>
-                    <div class="timeline-container left-container">
-                        <img src="/images/highschool-grad.jpg" alt="temp" />
-                        <div class="text-box">
-                            <h2 >Korea</h2>
-                            <small>2021-2022</small>
-                            <p>saklfjdlkafajksfjadskfsdfjaskldkj</p>
-                            <span class="left-container-arrow"></span>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
