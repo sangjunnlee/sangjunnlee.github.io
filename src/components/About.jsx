cc307c5afc7d3c132a36d3eeda4393e53e82ee9c
sangjunnlee <134983124+sangjunnlee@users.noreply.github.com>
2024-03-27 08:59:16 +0000

    Latest

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import profileImage from "../images/portrait.jpeg"
 
 function About() {
     return (
-        <div>
+        <div className="about-container">
             <h1>About</h1>
             <Carousel className="crsl" autoPlay infiniteLoop centerMode showArrows={false} showStatus={false} showThumbs={false}> 
                 <img src={profileImage} alt="image 1"/>
