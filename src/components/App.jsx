a71ebf08ba55122f6d776bf9b9f1a7d50eb39861
sangjunnlee <sjlee9920@gmail.com>
2024-03-27 18:27:23 +0900

    Feat: Go to top on refresh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
@@ -9,6 +9,21 @@ import NotFound from './NotFound';
 import '../style/App.css'
 
 function App() {
+    useEffect(() => {
+        // Function to scroll to the top
+        const scrollToTop = () => {
+            window.scrollTo(0, 0);
+        };
+
+        // Add event listener for page reload
+        window.addEventListener('beforeunload', scrollToTop);
+
+        // Remove event listener when component unmounts
+        return () => {
+            window.removeEventListener('beforeunload', scrollToTop);
+        };
+    }, []);
+
     return (
         <div className='App'>
             <BrowserRouter>
