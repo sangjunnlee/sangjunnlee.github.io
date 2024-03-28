88f982cffde84c69a1c7a576e5e5cd1e217b70ec
sangjunnlee <sjlee9920@gmail.com>
2024-03-28 22:08:34 +0900

    Feat: Resume

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,9 +2,9 @@ import React, {useState} from "react";
 import {Link, NavLink} from "react-router-dom";
 import './navbar.css'
 import { IoMdPerson } from "react-icons/io";
-import { IoCodeSlashOutline } from "react-icons/io5";
+import { IoCodeSlashOutline, IoDocumentOutline} from "react-icons/io5";
 import { MdComputer } from "react-icons/md";
-
+import resume from './SangJunLee_Resume.pdf';
 
 function NavBar() {
     const [menuOpen, setMenuOpen] = useState(false)
@@ -24,6 +24,9 @@ function NavBar() {
                 </li>
                 <li>
                     <NavLink to="/projects"><IoCodeSlashOutline className="icon"/>Projects</NavLink>
+                </li>
+                <li>
+                    <a href={resume} target="_blank" rel="noopener noreferrer"><IoDocumentOutline className="icon"/>Resume</a>
                 </li>
                 
             </ul>
