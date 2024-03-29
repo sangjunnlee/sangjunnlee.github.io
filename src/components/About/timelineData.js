5d3a684dc8f96633747a6a5965be274a3d7b43f9
sangjunnlee <sjlee9920@gmail.com>
2024-03-29 18:07:34 +0900

    checkpoint

--- src/components/About/timelineData.js
+++ src/components/About/timelineData.js
@@ -3,19 +3,19 @@ const timelineData = [
         imgSrc: "/assets/flags/guatemala.jpeg",
         imgAlt: "temp",
         country: "Guatemala",
-        date: "2021-2022"
+        date: "2003-2021"
     },
     {
         imgSrc: "/assets/flags/usa.png",
         imgAlt: "temp",
         country: "USA",
-        date: "2021-2022"
+        date: "2021-2023"
     },
     {
         imgSrc: "/assets/flags/korea.jpeg",
         imgAlt: "temp",
         country: "Korea",
-        date: "2021-2022"
+        date: "2023-present"
     },
     
 ];
