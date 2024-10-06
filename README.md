14741b5bf38c04c05241c95de006959f28c81fc0
sangjunnlee <sjlee9920@gmail.com>
2024-10-06 20:07:05 +0900

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# PersonalWebsite
-
-SangJun's Portfolio
--- src/components/About/imagePath.js
+++
@@ -1,8 +0,0 @@
-const imagePaths = [
-    '/assets/about/highschool-grad.JPG',
-    '/assets/about/korea-pic.jpg',
-    '/assets/about/korea.jpg',
-    '/assets/about/rokaf.PNG'
-]
-
-export {imagePaths};
