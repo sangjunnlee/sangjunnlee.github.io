6fe8b12efa6728e1e7e9a4e318ff478adcc046a6
sangjunnlee <sjlee9920@gmail.com>
2024-10-06 23:50:07 +0900

    Responsiveness to phone

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,40 +1,45 @@
-import React, {useState} from "react";
-import {Link, NavLink} from "react-router-dom";
-import './navbar.css'
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import './navbar.css';
 import { IoMdPerson } from "react-icons/io";
-import { IoCodeSlashOutline, IoDocumentOutline} from "react-icons/io5";
+import { IoCodeSlashOutline, IoDocumentOutline } from "react-icons/io5";
 import { MdComputer } from "react-icons/md";
 import resume from './SangJunLee_Resume.pdf';
 
 function NavBar() {
-    const [menuOpen, setMenuOpen] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        setMenuOpen(false);
+    }
+    
     return (
         <nav>
             <Link className="title" to="/"> SJL</Link>
-            <div className="menu" onClick={() => {
-                setMenuOpen(!menuOpen);
-            }}>               
+            <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
             <ul className={menuOpen ? "open" : ""}>
                 <li>
-                    <NavLink className='tab' to="/about">
-                        <IoMdPerson className="icon"/>
+                    <NavLink className='tab' to="/about" onClick={handleMenuClick}>
+                        <IoMdPerson className="icon" />
                         About Me
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/experience"><MdComputer className="icon"/> Experience</NavLink>
+                    <NavLink to="/experience" onClick={handleMenuClick}><MdComputer className="icon" /> Experience</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/projects"><IoCodeSlashOutline className="icon"/> Projects</NavLink>
+                    <NavLink to="/projects" onClick={handleMenuClick}><IoCodeSlashOutline className="icon" /> Projects</NavLink>
                 </li>
                 <li>
-                    <a href={resume} target="_blank" rel="noopener noreferrer"><IoDocumentOutline className="icon"/>Resume</a>
+                    <a href={resume} target="_blank" rel="noopener noreferrer"><IoDocumentOutline className="icon" />Resume</a>
                 </li>
-                
             </ul>
         </nav>
-    )
-} 
+    );
+}
 
-export default NavBar
+export default NavBar;
