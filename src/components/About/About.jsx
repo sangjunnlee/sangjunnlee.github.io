72d26f422f1a3102cc5aecc3a3526d285cc536de
sangjunnlee <sjlee9920@gmail.com>
2024-10-07 20:39:21 +0900

    Change bg color

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -20,12 +20,17 @@ function About() {
                     </Carousel>
                     <div className="text-about">
                         <h1>
-                            About Me!
+                            <u><i>Who am I?</i></u>
                         </h1>
-                        <p style={{fontSize: 'medium'}}>
-                            Hi! I'm SangJun Lee, currently a junior at UC Berkeley studying Electrical Engineering and Computer Science (EECS) 🚀🚀. <br /> <br />
-                            I have had the privilege of being able to call three different countries my home 🌏: Guatemala, Korea, and the United States. My diverse childhood has endowed me with a broad perspective and deeper appreciation of different cultures. <br /><br />
-                            Some of my hobbies include traveling, sports, and programming. I love exploring new places around the world. Furthermore, I enjoy playing sports as a way to relieve myself from studying and work. I'm especially in love with basketball 🏀 and football ⚽️. <br /><br />
+                        <p>
+                            Hi! I'm a junior at <u>UC Berkeley</u> studying <u>Electrical Engineering and Computer Science</u> 💻. <br /> <br />
+
+                            I spent my childhood in Guatemala 🇬🇹, flew to the United States for college 🇺🇸, and now am serving in the Air Force in South Korea 🇰🇷.<br /><br />
+
+                            Outside of my studies, I love traveling 🛬 and learning about new cultures, playing sports, especially basketball, soccer, and tennis, and tasting new cuisines with my friends and family.
+                            
+                            
+                            I love exploring new places around the world. Furthermore, I enjoy playing sports as a way to relieve myself from studying and work. I'm especially in love with basketball 🏀 and football ⚽️. <br /><br />
                             Professionally 💻, I specialize in backend development, but my journey in tech is not limited to one domain. I've also ventured in full stack development and the process of training and fine-tuning AI models. Embracing challenges and new opportunities to learn in this dynamic field is what drives my passion towards programming. I love working with new people and embracing new challenges. <br /><br />
                             Feel free to contact me if you want to know more! 
                         </p>
