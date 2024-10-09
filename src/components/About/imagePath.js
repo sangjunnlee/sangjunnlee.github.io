103bf015c85122c907b7224524db0afe94ab653b
sangjunnlee <sjlee9920@gmail.com>
2024-10-09 18:14:34 +0900

    Fix grad pic issue

--- src/components/About/imagePath.js
+++ src/components/About/imagePath.js
@@ -1,5 +1,5 @@
 const imagePaths = [
-    '/assets/about/highschool-grad.jpg',
+    '/assets/about/highschool.jpg',
     '/assets/about/korea-pic.jpg',
     '/assets/about/korea.jpg',
     '/assets/about/rokaf.PNG'
